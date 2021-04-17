c8925a82099af87f019f14645ce0dbb2f06d4bd3
khayliang <khayliang@gmail.com>
2021-04-17 15:33:21 +0800

    added routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React from "react"
-import OrbitPage from "./pages/test-orbit"
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
+
+import NoMatch from "./pages/NoMatch"
 import { OrbitProvider } from "./common/context/Orbit"
+import routes from "./routes"
 
 function App() {
   return (
     <>
       <OrbitProvider>
-        <OrbitPage />
+        <Router>
+          <Switch>
+            {routes.map((route) => (
+              <Route
+                key={`route-${route.path}`}
+                exact={route.exact}
+                path={route.path}
+                component={route.component}
+              />
+            ))}
+            <Route path="*" component={NoMatch} />
+          </Switch>
+        </Router>
       </OrbitProvider>
     </>
   )
---
+++ src/routes/index.js
@@ -0,0 +1,12 @@
+import TestOrbit from "../pages/TestOrbit"
+
+// Keep adding routes here
+const routes = [
+  {
+    path: "/test",
+    exact: true,
+    component: TestOrbit,
+  },
+]
+
+export default routes
